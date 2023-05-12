cc0a22f377045026ceef42cf754adb8ca9c6fc1b
cary-rowen <manchen_0528@outlook.com>
2023-05-12 15:30:00 +0800

    Improvements were made to the clipboard editor.

--- addon/globalPlugins/clipboardEnhancement/clipEditor.py
+++ addon/globalPlugins/clipboardEnhancement/clipEditor.py
@@ -3,162 +3,176 @@ import wx
 import os
 import re
 from . import reReplace
+
 class MyFrame(wx.Frame):
 	def __init__(self, *args, **kw):
-		wx.Frame.__init__(self, *args, size=(600,460), **kw)
-		self.isExit = False
+		wx.Frame.__init__(self, *args, size=(600, 460), **kw)
+		self.is_exit = False
 		pnl = wx.Panel(self)
 		self.edit = wx.TextCtrl(pnl, pos=(0, 0), style=wx.TE_PROCESS_TAB|wx.TE_MULTILINE|wx.TE_RICH2|wx.HSCROLL)
 		menubar = wx.MenuBar()
-		menu = wx.Menu()
-		menu.Append(wx.ID_OPEN, '打开(&O)\tCtrl+O', '更新剪贴板数据')
-		update = menu.Append(-1, '更新(&U)\tAlt+U')
-		menu.Append(wx.ID_SAVE, '更新并关闭(&X)\tAlt+X')
-		menu.Append(wx.ID_SAVEAS, '另存为(&A)\tAlt+A', '保存项目')
-		menu.Append(wx.ID_EXIT, '关闭(&Q)\tAlt+Q', '退出程序')
-		self.edit.Bind(wx.EVT_KEY_DOWN, self.OnKeydown)
-		self.Bind(wx.EVT_MENU, self.OnOpen, id=wx.ID_OPEN)
-		self.Bind(wx.EVT_MENU, self.OnUpdate, update)
-		self.Bind(wx.EVT_MENU, self.OnSave, id=wx.ID_SAVE)
-		self.Bind(wx.EVT_MENU, self.OnSaveAs, id=wx.ID_SAVEAS)
-		self.Bind(wx.EVT_MENU, self.OnExit, id=wx.ID_EXIT)
-		menubar.Append(menu, '文件(&F)')
-		menu2 = wx.Menu()
-		find = menu2.Append(-1, '查找(&F)\tCtrl+F')
-		self.Bind(wx.EVT_MENU, self.OnShowFind, find)
-		replace = menu2.Append(-1, '替换(&H)\tCtrl+H')
-		self.Bind(wx.EVT_MENU,self.OnShowReplace, replace)
-		next = menu2.Append(-1, '查找下一个\tF3')
-		self.Bind(wx.EVT_MENU, self.OnNext, next)
-		goto = menu2.Append(-1, '转到(&G)\tCtrl+G')
-		self.Bind(wx.EVT_MENU, self.OnGoto, goto)
-		r=menu2.Append(-1, '正则替换(&R)\tCtrl+R')
-		self.Bind(wx.EVT_MENU, self.OnRe, r)
-		menubar.Append(menu2, '编辑(&E)')
+		file_menu = wx.Menu()
+		file_menu.Append(wx.ID_OPEN, '打开(&O)\tCtrl+O', '打开文件')
+		update = file_menu.Append(-1, '更新(&U)\tAlt+U', '更新剪贴板数据')
+		file_menu.Append(wx.ID_SAVE, '更新并关闭(&X)\tAlt+X', '更新剪贴板数据并关闭剪贴板编辑器')
+		file_menu.Append(wx.ID_SAVEAS, '另存为(&A)\tAlt+A', '另存为文件')
+		file_menu.Append(wx.ID_EXIT, '退出(&C)\tAlt+C', '关闭剪贴板编辑器')
+		self.edit.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
+		self.Bind(wx.EVT_MENU, self.on_open, id=wx.ID_OPEN)
+		self.Bind(wx.EVT_MENU, self.on_update, update)
+		self.Bind(wx.EVT_MENU, self.on_save, id=wx.ID_SAVE)
+		self.Bind(wx.EVT_MENU, self.on_save_as, id=wx.ID_SAVEAS)
+		self.Bind(wx.EVT_MENU, self.on_exit, id=wx.ID_EXIT)
+		menubar.Append(file_menu, '文件(&F)')
+		edit_menu = wx.Menu()
+		find = edit_menu.Append(-1, '查找(&F)\tCtrl+F', '查找文本')
+		self.Bind(wx.EVT_MENU, self.on_show_find, find)
+		replace = edit_menu.Append(-1, '替换(&H)\tCtrl+H', '替换文本')
+		self.Bind(wx.EVT_MENU, self.on_show_replace, replace)
+		next_item = edit_menu.Append(-1, '查找下一个(&N)\tF3', '查找下一个匹配项')
+		self.Bind(wx.EVT_MENU, self.on_next, next_item)
+		goto_item = edit_menu.Append(-1, '转到(&G)\tCtrl+G', '转到指定行')
+		self.Bind(wx.EVT_MENU, self.on_goto, goto_item)
+		previous_item = edit_menu.Append(-1, '查找上一个(&P)\tShift+F3', '查找上一个匹配项')
+		self.Bind(wx.EVT_MENU, self.on_previous, previous_item)
+		replacement = edit_menu.Append(-1, '正则替换(&R)\tCtrl+R', '使用正则表达式替换文本')
+		self.Bind(wx.EVT_MENU, self.on_replacement, replacement)
+		menubar.Append(edit_menu, '编辑(&E)')
 		self.SetMenuBar(menubar)
-		self.finddata = wx.FindReplaceData(wx.FR_DOWN)
+		self.find_data = wx.FindReplaceData(wx.FR_DOWN)
 		self.edit.SetFont(wx.FFont(7, wx.DEFAULT, wx.NORMAL))
-		self.Bind(wx.EVT_SIZE, self.OnSize)
-	def OnKeydown(self, evt):
+		self.Bind(wx.EVT_SIZE, self.on_size)
+
+	def on_key_down(self, evt):
 		code = evt.GetKeyCode()
 		if code == wx.WXK_ESCAPE:
 			self.Show(False)
-			p = self.edit.GetInsertionPoint()
-			xy = self.edit.PositionToXY(p)
-			if xy[0]:
-				self.setClipboardPosition(xy[1], xy[2])
+			position = self.edit.GetInsertionPoint()
+			XY = self.edit.PositionToXY(position)
+			if XY[0]:
+				self.set_clipboard_position(XY[1], XY[2])
 		evt.Skip()
 
-	def setClipboardPosition(self, x, y):
+	def set_clipboard_position(self, x, y):
 		pass
 
-	def OnUpdate(self, evt):
+	def on_update(self, evt):
 		text = self.edit.GetValue()
 		if not text:
-			self.clearClipboard()
+			self.clear_clipboard()
 		else:
 			api.copyToClip(text)
 
-	def OnRe(self, evt):
+	def on_replacement(self, evt):
 		dlg = reReplace.ReDialog(self)
 		if dlg.ShowModal() == wx.ID_OK:
 			pattern = dlg.cb.GetValue()
 			aim = dlg.cb2.GetValue()
-			self.edit.SetValue(re.sub(pattern, aim, self.edit.GetValue()))
-			self.SaveHistory(pattern, aim)
-		dlg.Destroy()
-	def SaveHistory(self, pattern, aim):
+			try:
+				self.edit.SetValue(re.sub(pattern, aim, self.edit.GetValue()))
+				self.save_history(pattern, aim)
+			except re.error as e:
+				error_message = "替换操作失败：{}".format(str(e))
+				wx.MessageBox(error_message, "错误", wx.OK | wx.ICON_ERROR)
+			dlg.Destroy()
+
+	def save_history(self, pattern, aim):
 		pass
-	def OnNext(self, e):
-		if not self.finddata.GetFindString():
-			self.OnShowFind(wx.EVT_MENU)
+
+	def on_next(self, e):
+		self.find_data.SetFlags(self.find_data.GetFlags() | wx.FR_DOWN)  # 启用向下查找
+		if not self.find_data.GetFindString():
+			self.on_show_find(wx.EVT_MENU)
 		else:
-			self.OnFind(wx.EVT_FIND)
-	def OnShowFind(self, event):
-		dlg = wx.FindReplaceDialog(self, self.finddata, '查找')
-		dlg.Bind(wx.EVT_FIND, self.OnFind)
-		dlg.Bind(wx.EVT_FIND_NEXT, self.OnFind)
-		dlg.Bind(wx.EVT_FIND_CLOSE, self.OnFindClose)
+			self.on_find(wx.EVT_FIND)
+	def on_previous(self, event):
+		self.find_data.SetFlags(self.find_data.GetFlags() & ~wx.FR_DOWN)   # 启用向上查找
+		if not self.find_data.GetFindString():
+			self.on_show_find(wx.EVT_MENU)
+		else:
+			self.on_find(wx.EVT_FIND)
+
+	def on_show_find(self, event):
+		dlg = wx.FindReplaceDialog(self, self.find_data, '查找')
+		dlg.Bind(wx.EVT_FIND, self.on_find)
+		dlg.Bind(wx.EVT_FIND_NEXT, self.on_find)
+		dlg.Bind(wx.EVT_FIND_CLOSE, self.on_find_close)
 		dlg.Show()
-	def OnShowReplace(self, event):
-		dlg = wx.FindReplaceDialog(self, self.finddata, '替换', wx.FR_REPLACEDIALOG)
-		dlg.Bind(wx.EVT_FIND, self.OnFind)
-		dlg.Bind(wx.EVT_FIND_REPLACE, self.OnReplace)
-		dlg.Bind(wx.EVT_FIND_REPLACE_ALL, self.OnReplaceAll)
-		dlg.Bind(wx.EVT_FIND_CLOSE, self.OnFindClose)
+
+	def on_show_replace(self, event):
+		dlg = wx.FindReplaceDialog(self, self.find_data, '替换', wx.FR_REPLACEDIALOG)
+		dlg.Bind(wx.EVT_FIND, self.on_find)
+		dlg.Bind(wx.EVT_FIND_REPLACE, self.on_replace)
+		dlg.Bind(wx.EVT_FIND_REPLACE_ALL, self.on_replace_all)
+		dlg.Bind(wx.EVT_FIND_CLOSE, self.on_find_close)
 		dlg.Show()
-	def OnFind(self, event):
-		findtext = self.finddata.GetFindString()
+
+	def on_find(self, event):
+		find_text = self.find_data.GetFindString()
 		offset = self.edit.GetInsertionPoint()
-		if self.finddata.GetFlags() & wx.FR_DOWN:
-			if self.edit.GetStringSelection().lower()==findtext.lower():
-				offset+=len(findtext)
-			aimtext=self.edit.GetRange(offset, self.edit.GetLastPosition())
-			if not self.finddata.GetFlags() & wx.FR_MATCHCASE:
-				m= re.search(findtext, aimtext, re.I)
-				if m:
-					start = m.span()[0]
-				else:
-					start=-1
+		if self.find_data.GetFlags() & wx.FR_DOWN:
+			if self.edit.GetStringSelection().lower() == find_text.lower():
+				offset += len(find_text)
+			aim_text = self.edit.GetRange(offset, self.edit.GetLastPosition())
+			if not self.find_data.GetFlags() & wx.FR_MATCHCASE:
+				start = aim_text.lower().find(find_text.lower())
 			else:
-				start = aimtext.find(findtext)
+				start = aim_text.find(find_text)
 		else:
-			aimtext = self.edit.GetRange(0, offset)
-			if not self.finddata.GetFlags() & wx.FR_MATCHCASE:
-				m= re.findall(findtext, aimtext, re.I)
-				if m:
-					findtext = m[-1]
-			start = aimtext.rfind(findtext)
+			aim_text = self.edit.GetRange(0, offset)
+			if not self.find_data.GetFlags() & wx.FR_MATCHCASE:
+				start = aim_text.lower().rfind(find_text.lower())
+			else:
+				start = aim_text.rfind(find_text)
 		if start > -1:
-			start += offset if self.finddata.GetFlags()&wx.FR_DOWN else 0
-			self.edit.SetSelection(start, start+len(findtext))
+			start += offset if self.find_data.GetFlags() & wx.FR_DOWN else 0
+			self.edit.SetSelection(start, start + len(find_text))
 		else:
-			wx.MessageBox('找不到\"{}\"'.format(findtext), '提示')
-	def OnReplace(self, event):
-		findtext = self.finddata.GetFindString()
-		replacetext = self.finddata.GetReplaceString()
+			wx.MessageBox('找不到\"{}\"'.format(find_text), '提示')
+
+	def on_replace(self, event):
+		find_text = self.find_data.GetFindString()
+		replace_text = self.find_data.GetReplaceString()
 		offset = self.edit.GetInsertionPoint()
 		text = self.edit.GetRange(offset, self.edit.GetLastPosition())
-		if not self.finddata.GetFlags() & wx.FR_MATCHCASE:
-			m = re.search(findtext, text, re.I)
-			start = m.span()[0] if m else -1
+		if not self.find_data.GetFlags() & wx.FR_MATCHCASE:
+			start = text.lower().find(find_text.lower())
 		else:
-			start = text.find(findtext)
+			start = text.find(find_text)
 		if start > -1:
 			start += offset
-			self.edit.Replace(start, start+len(findtext), replacetext)
-	def OnReplaceAll(self, event):
-		findtext = self.finddata.GetFindString()
-		replacetext = self.finddata.GetReplaceString()
+			self.edit.Replace(start, start + len(find_text), replace_text)
+
+	def on_replace_all(self, event):
+		find_text = self.find_data.GetFindString()
+		replace_text = self.find_data.GetReplaceString()
 		offset = 0
-		if not self.finddata.GetFlags() & wx.FR_MATCHCASE:
-			while True:
-				text = self.edit.GetRange(offset, self.edit.GetLastPosition())
-				m = re.search(findtext, text, re.I)
-				start = m.span()[0] if m else -1
-				if start == -1:
-					break
-				start += offset 
-				self.edit.Replace(start, start+len(findtext), replacetext)
-				offset = self.edit.GetInsertionPoint()
-			return
+		count = 0
 		while True:
 			text = self.edit.GetRange(offset, self.edit.GetLastPosition())
-			start = text.find(findtext)
+			start = text.find(find_text)
 			if start == -1:
 				break
 			start += offset 
-			self.edit.Replace(start, start+len(findtext), replacetext)
+			self.edit.Replace(start, start + len(find_text), replace_text)
 			offset = self.edit.GetInsertionPoint()
-	def OnFindClose(self, event):
+			count += 1
+		if count > 0:
+			wx.MessageBox('已替换{}个匹配项'.format(count), '提示')
+		else:
+			wx.MessageBox('找不到\"{}\"'.format(find_text), '提示')
+
+	def on_find_close(self, event):
 		event.GetDialog().Destroy()
-	def OnGoto(self, event):
+
+	def on_goto(self, event):
 		line = self.edit.PositionToXY(self.edit.GetInsertionPoint())[2]
 		dlg = wx.NumberEntryDialog(self, '', '行号(&L)', '转到指定行', line+1, 1, self.edit.GetNumberOfLines())
 		if dlg.ShowModal() == wx.ID_OK:
-			point = self.edit.XYToPosition(0, dlg.GetValue()-1)
+			point = self.edit.coordinateToPosition(0, dlg.GetValue()-1)
 			self.edit.SetInsertionPoint(point)
-	def OnOpen(self, event):
+
+	def on_open(self, event):
 		wildcard = 'txt文件(*.txt)|*.txt|' \
 			'所有文件 (*.*)|*.*'
 		filename = ''
@@ -173,19 +187,19 @@ class MyFrame(wx.Frame):
 				self.edit.SetValue(f.read())
 		except Exception as e:
 			try:
-				with open(filename, encoding = 'utf-8') as f:
+				with open(filename, encoding='utf-8') as f:
 					self.edit.SetValue(f.read())
 			except Exception as e:
 				wx.MessageBox(str(e), '错误')
-		self.OpenFile(ext)
-	def Destroy(self):
-		if not self.isExit:
+
+	def destroy(self):
+		if not self.is_exit:
 			self.Show(False)
 			return 0
 		else:
 			return super().Destroy()
 
-	def OnSaveAs(self, evt):
+	def on_save_as(self, evt):
 		text = self.edit.GetValue()
 		wildcard = '文本文件 (*.txt)|*.txt|' \
 			'所有文件 (*.*)|*.*'
@@ -199,42 +213,23 @@ class MyFrame(wx.Frame):
 		with open(filepath, 'wb') as f:
 			f.write(text.encode('utf8'))
 
-	def OnSave(self, event):
+
+	def on_save(self, event):
 		text = self.edit.GetValue()
 		if text:
 			api.copyToClip(text)
 		else:
-			self.clearClipboard()
+			self.clear_clipboard()
 		self.Show(False)
 
-	def saveFile(self, filename, text):
-		wildcard = '文本文件 (*.txt)|*.txt|' \
-			'所有文件 (*.*)|*.*'
-		filepath = ''
-		dlg = wx.FileDialog(self, message='文件另存为...', defaultDir='', defaultFile=filename, wildcard=wildcard, style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
-		if dlg.ShowModal() == wx.ID_OK:
-			filepath = dlg.GetPath()
-		dlg.Destroy()
-		if not filepath:
-			return
-		with open(filepath, 'wb') as f:
-			f.write(text.encode('utf8'))
-		self.SetTitle('-'.join((os.path.basename(filepath), self.name)))
-		if self.filename and not self.edit.IsModified():
-			self.CheckLatest100()
-		self.filename = filepath
-		self.mark= []
-		self.index = -1
-		self.edit.DiscardEdits()
-	def OnExit(self, event):
+	def on_exit(self, event):
 		self.Show(False)
-	def OnSize(self, event):
+
+	def on_size(self, event):
 		self.edit.SetSize(self.GetClientSize())
 		event.Skip()
 
-
-	# 清空剪贴板
-	def clearClipboard(self):
+	def clear_clipboard(self):
 		import winUser
-		with winUser.openClipboard():
-			winUser.emptyClipboard()
+		with winUser.open_clipboard():
+			winUser.empty_clipboard()
